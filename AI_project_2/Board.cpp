92476d671c0f970a18fb64e9af8220704bfc2abb
JohnHurtTheFool <FryeES1@gcc.edu>
2015-11-14 17:00:02 -0500

    Worked on board class

--- AI_project_2/Board.cpp
+++ AI_project_2/Board.cpp
@@ -1,13 +1,66 @@
 #include "Board.h"
-
+using std::vector;
 
 Board::Board(void)
 {
+	Move::square A1 = {0,0};
+	whiteKingMoved = false;
+	blackKingMoved = false;
+	last = new Move(A1,A1);
+
+	//all the empty squares
+	content empty = {true,true,P};
+	for(int i = 0; i < NUM_FILES; i++)
+		for(int j = 3; j < 7; j++)
+			squares[i][j] = empty;
+
+	//Pawns
+	content whitePawn = {false,true,P};
+	content blackPawn = {false,true,P};
+	for(int i = 0; i < NUM_FILES; i++)
+			squares[i][2] = whitePawn;
+	for(int i = 0; i < NUM_FILES; i++)
+			squares[i][7] = blackPawn;
+	//Back ranks.
+	content a1 = {false,true,R};
+	content b1 = {false,true,N};
+	content c1 = {false,true,B};
+	content d1 = {false,true,Q};
+	content e1 = {false,true,K};
+	content f1 = {false,true,B};
+	content g1 = {false,true,N};
+	content h1 = {false,true,R};
+	content a8 = {false,false,R};
+	content b8 = {false,false,N};
+	content c8 = {false,false,B};
+	content d8 = {false,false,Q};
+	content e8 = {false,false,K};
+	content f8 = {false,false,B};
+	content g8 = {false,false,N};
+	content h8 = {false,false,R};
+	squares[0][0]= a1;
+	squares[1][0]= b1;
+	squares[2][0]= c1;
+	squares[3][0]= d1;
+	squares[4][0]= e1;
+	squares[5][0]= f1;
+	squares[6][0]= g1;
+	squares[7][0]= h1;
+	squares[0][0]= a1;
+	squares[1][7]= b8;
+	squares[2][7]= c8;
+	squares[3][7]= d8;
+	squares[4][7]= e8;
+	squares[5][7]= f8;
+	squares[6][7]= g8;
+	squares[7][7]= h8;
 }
 
 
 Board::~Board(void)
 {
+	if(last)
+		delete last;
 }
 bool Board::isLegal(Move m)
 {
@@ -22,32 +75,47 @@ Board Board::result (Move m)
 int Board::materialCount()
 {
 	int score;
-	for(int i = 0; i < NUM_RANKS; i++)
-		for(int j = 0; j < NUM_FILES; j++)
-			if(!squares[i][j].empty)
+	for(int i = 0; i < NUM_FILES; i++)
+		for(int j = 0; j < NUM_RANKS; j++)
+			if(!squares[i][j].empty)//If there is a piece.
 			{
-				if(squares[i][j].white)
+				if(squares[i][j].white)//If the piece is white.
 				{
 					if(squares[i][j].piece==Q)
-						score+=Q_V;
+						score+=boardNS::Q_V;
 					else if(squares[i][j].piece==R)
-						score+=R_V;
+						score+=boardNS::R_V;
 					else if(squares[i][j].piece==B)
-						score+=B_V;
+						score+=boardNS::B_V;
 					else if(squares[i][j].piece==N)
-						score+=N_V;
+						score+=boardNS::N_V;
 					else if(squares[i][j].piece==P)
-						score+=P_V;
+						score+=boardNS::P_V;
 				}
 				else
 				{
+					if(squares[i][j].piece==Q)
+						score-=boardNS::Q_V;
+					else if(squares[i][j].piece==R)
+						score-=boardNS::R_V;
+					else if(squares[i][j].piece==B)
+						score-=boardNS::B_V;
+					else if(squares[i][j].piece==N)
+						score-=boardNS::N_V;
+					else if(squares[i][j].piece==P)
+						score-=boardNS::P_V;
 				}
 			}
-	//Please add me.
-	return 0;//Placeholder
+	return score;
 }
-int weightedCount()
+int Board::weightedCount()
 {
 	//Please add me
 	return 0;//Placeholder
+}
+std::vector<Move> Board::getLegal()
+{
+	vector<Move> legal;
+	//Please add me
+	return legal;
 }
--- AI_project_2/Board.h
+++ AI_project_2/Board.h
@@ -1,14 +1,18 @@
 #pragma once
 #include "Move.h"
-class Board
+#include <vector>
+namespace boardNS
 {
-public:
-	static const enum pieces{K,Q,R,B,N,P};//(Pawns are not really pieces, but we include them anyway.)
 	const double Q_V = 9.2;
 	const double R_V = 5.0;
 	const double B_V = 3.3;
 	const double N_V = 3.0;
 	const double P_V = 1.0;
+}
+class Board
+{
+public:
+	static const enum pieces{K,Q,R,B,N,P};//(Pawns are not really pieces, but we include them anyway.)
 	struct content
 	{
 		bool empty;
@@ -17,16 +21,18 @@ public:
 	};
 	static const int NUM_RANKS = 8;
 	static const int NUM_FILES = 8;
-	bool isLegal(Move m);//Whether the move is legal
+	
 	Board result (Move m);//The board after the move is made.
-	int materialCount();
+	int materialCount();//Positive for white has more material.
 	int weightedCount();//Our heuristic. Could be based on material count and other factors
-	Board(void);
+	std::vector<Move> getLegal();//Returns a vector of all legal moves.
+	Board(void);//Creates board at initial position
 	~Board(void);
 private:
-	content squares[NUM_RANKS][NUM_FILES];//An array of the contents in each square.
+	bool isLegal(Move m);//Whether the move is legal
+	content squares[NUM_FILES][NUM_RANKS];//An array of the contents in each square. (0,0) is a1, (7,0) is h1, (0,7) is a8, etc
 	bool whiteKingMoved;//We keep track of whether the kings have moved, so that we know if they can castle
 	bool blackKingMoved;
-	Move last;//For en passant.
+	Move* last;//For en passant.
 };
 
