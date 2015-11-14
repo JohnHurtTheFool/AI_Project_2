de63dc5711f412fc452afd6724e00c45ae658fcb
JohnHurtTheFool <FryeES1@gcc.edu>
2015-11-14 15:57:28 -0500

    worked on Board class

--- AI_project_2/Board.cpp
+++ AI_project_2/Board.cpp
@@ -19,8 +19,30 @@ Board Board::result (Move m)
 	//Please add me.
 	return *this;//Placeholder
 }
-int Board::meterialCount()
+int Board::materialCount()
 {
+	int score;
+	for(int i = 0; i < NUM_RANKS; i++)
+		for(int j = 0; j < NUM_FILES; j++)
+			if(!squares[i][j].empty)
+			{
+				if(squares[i][j].white)
+				{
+					if(squares[i][j].piece==Q)
+						score+=Q_V;
+					else if(squares[i][j].piece==R)
+						score+=R_V;
+					else if(squares[i][j].piece==B)
+						score+=B_V;
+					else if(squares[i][j].piece==N)
+						score+=N_V;
+					else if(squares[i][j].piece==P)
+						score+=P_V;
+				}
+				else
+				{
+				}
+			}
 	//Please add me.
 	return 0;//Placeholder
 }
--- AI_project_2/Board.h
+++ AI_project_2/Board.h
@@ -4,6 +4,11 @@ class Board
 {
 public:
 	static const enum pieces{K,Q,R,B,N,P};//(Pawns are not really pieces, but we include them anyway.)
+	const double Q_V = 9.2;
+	const double R_V = 5.0;
+	const double B_V = 3.3;
+	const double N_V = 3.0;
+	const double P_V = 1.0;
 	struct content
 	{
 		bool empty;
@@ -14,7 +19,7 @@ public:
 	static const int NUM_FILES = 8;
 	bool isLegal(Move m);//Whether the move is legal
 	Board result (Move m);//The board after the move is made.
-	int meterialCount();
+	int materialCount();
 	int weightedCount();//Our heuristic. Could be based on material count and other factors
 	Board(void);
 	~Board(void);
