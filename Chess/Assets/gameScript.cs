5732c1707f757bfb5ea1b2fac55f7195d9e7bd97
curtismj1 <curtismj1@gcc.edu>
2015-11-27 14:13:09 -0500

    Alright so here is the base Unity application. Right now, if you click anywhere on the board, it will log the position you clicked on the chess board to your console. There is only one script which contains x and y positions for where white clicked. So I suppose the next stage is to incorporate the logic we have in the c++ files into c# scripts.

---
+++ Chess/Assets/gameScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameScript : MonoBehaviour {
+	public bool isWhitesTurn;
+	public GameObject board;
+	float squareWidth;
+	float squareHeight;
+	// Use this for initialization
+	void Start () {
+		isWhitesTurn = true;
+		squareWidth = board.GetComponent<RectTransform> ().rect.width / 8.0f;
+		squareHeight = board.GetComponent<RectTransform> ().rect.height / 8.0f;
+	}
+	public void takeTurn(){
+		if (isWhitesTurn) {
+			int x = 1 + (int)(((board.GetComponent<RectTransform>().rect.width/2 
+			          + Input.mousePosition.x - board.transform.position.x))/ squareWidth);
+			int y = 1 + (int)((board.GetComponent<RectTransform>().rect.height/2
+			              + Input.mousePosition.y - board.transform.position.y)/squareHeight);
+			Debug.Log("X Pos: " + x + "Y Pos: " + y);
+			//Debug.Log ("Board Width " + board.GetComponent<RectTransform>().rect.width +
+			           //"Board Height " + board.GetComponent<RectTransform>().rect.height);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
